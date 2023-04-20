5021a3040ab2ac59997ee9b0b4e5f0af24593d51
altcode255 <anonte3p5usu@gmail.com>
2023-04-20 15:19:51 -0700

    Change shuuen.log -> shuuen_recent.txt

--- shuuen.py
+++ shuuen.py
@@ -13,13 +13,13 @@ client = tweepy.Client(consumer_key=keys["consumer_key"], consumer_secret=keys["
 def getQuote():
 	with open(directory + "/shuuen.txt", "r", encoding="utf-8") as f:
 		quotes = f.read().splitlines()
-	with open(directory + "/shuuen.log", "r", encoding="utf-8") as f:
+	with open(directory + "/shuuen_recent.txt", "r", encoding="utf-8") as f:
 		log = f.read().splitlines()
 	filtered_quotes = [quote for quote in quotes if quote not in log]
 	random_quote = random.choice(filtered_quotes)
 	log.pop(0)
 	log.append(random_quote)
-	with open(directory + "/shuuen.log", "w", encoding="utf-8") as f:
+	with open(directory + "/shuuen_recent.txt", "w", encoding="utf-8") as f:
 		f.write('\n'.join(log))
 	return random_quote
 
