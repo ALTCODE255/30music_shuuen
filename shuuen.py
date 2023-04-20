ecd5207bcffdd9295716d601a0523a70010a6b98
altcode255 <anonte3p5usu@gmail.com>
2023-04-20 15:38:02 -0700

    Autoformat using black + autofill empty string

--- shuuen.py
+++ shuuen.py
@@ -6,21 +6,30 @@ import random
 directory = os.path.realpath(os.path.dirname(__file__))
 
 with open(directory + "/creds.json", "r") as credentials:
-	keys = json.load(credentials)
+    keys = json.load(credentials)
 
-client = tweepy.Client(consumer_key=keys["consumer_key"], consumer_secret=keys["consumer_secret"], access_token=keys["access_token"], access_token_secret=keys["access_token_secret"])
+client = tweepy.Client(
+    consumer_key=keys["consumer_key"],
+    consumer_secret=keys["consumer_secret"],
+    access_token=keys["access_token"],
+    access_token_secret=keys["access_token_secret"],
+)
+
 
 def getQuote() -> str:
-	with open(directory + "/shuuen.txt", "r", encoding="utf-8") as f:
-		quotes = f.read().splitlines()
-	with open(directory + "/shuuen_recent.txt", "r", encoding="utf-8") as f:
-		log = f.read().splitlines()
-	filtered_quotes = [quote for quote in quotes if quote not in log]
-	random_quote = random.choice(filtered_quotes)
-	log.pop(0)
-	log.append(random_quote)
-	with open(directory + "/shuuen_recent.txt", "w", encoding="utf-8") as f:
-		f.write('\n'.join(log))
-	return random_quote
+    with open(directory + "/shuuen.txt", "r", encoding="utf-8") as f:
+        quotes = f.read().splitlines()
+    with open(directory + "/shuuen_recent.txt", "r", encoding="utf-8") as f:
+        log = f.read().splitlines()
+        if len(log) < 11:
+            log = [""] * (11 - len(log)) + log
+    filtered_quotes = [quote for quote in quotes if quote not in log]
+    random_quote = random.choice(filtered_quotes)
+    log.pop(0)
+    log.append(random_quote)
+    with open(directory + "/shuuen_recent.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(log))
+    return random_quote
+
 
 client.create_tweet(text=getQuote())
